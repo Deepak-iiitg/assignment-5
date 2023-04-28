abbc6f45cf299bc7a5c44ad9e919361b34658236
Deepak-iiitg <94068572+Deepak-iiitg@users.noreply.github.com>
2023-04-28 20:08:27 +0530

    Add files via upload

---
+++ assignment9/index.js
@@ -0,0 +1,53 @@
+const submit = document.getElementById("submit");
+submit.addEventListener('click',(e)=>{
+     e.preventDefault();
+     console.log("listener is called");
+    const inputs = document.getElementById("item");
+      const ul = document.getElementById("items");
+      const li = document.createElement("li");
+      
+      const btn = document.createElement("button");
+      btn.className = "btn btn-danger btn-sm float-right delete";
+      btn.appendChild(document.createTextNode(' X'));
+      const text = document.createTextNode(inputs.value);
+      li.appendChild(text);
+      li.className = 'list-group-item';
+      li.append(btn);
+      ul.appendChild(li);
+
+
+})
+const del = document.getElementById("delete");
+del.addEventListener("click",(e)=>{
+    e.preventDefault();
+    const ul = document.getElementById("items");
+    const list = document.querySelectorAll("li");
+    const li = list[list.length-1];
+    ul.removeChild(li);
+})
+const itemList = document.getElementById("items");
+const filter = document.getElementById("filter");
+filter.addEventListener('keyup', filterItems);
+function filterItems(e){
+  // convert text to lowercase
+  var text = e.target.value.toLowerCase();
+  // Get lis
+  var items = itemList.getElementsByTagName('li');
+  // Convert to an array
+  Array.from(items).forEach(function(item){
+    var itemName = item.firstChild.textContent;
+    if(itemName.toLowerCase().indexOf(text) != -1){
+      item.style.display = 'block';
+    } else {
+      item.style.display = 'none';
+    }
+  });
+}
+function removeItem(e){
+    if(e.target.classList.contains('delete')){
+      if(confirm('Are You Sure?')){
+        var li = e.target.parentElement;
+        itemList.removeChild(li);
+      }
+    }
+}
